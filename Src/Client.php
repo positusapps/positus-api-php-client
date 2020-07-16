321b053d7f45ff419f62737a9145e970aa22f4cf
caiokawasaki <caiokawasaki@gmail.com>
2020-07-16 18:21:57 -0300

    Changed folders structure

---
+++ Src/Client.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Positus;
+
+class Client
+{
+    public function __construct()
+    {
+        return new Client();
+    }
+
+    public function init()
+    {
+        return 'test';
+    }
+}
