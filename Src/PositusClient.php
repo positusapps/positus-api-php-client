66075ca76d90588fa5d61e9b7fe99bebfaabefb6
caiokawasaki <caiokawasaki@gmail.com>
2020-07-16 17:31:57 -0300

    First Commit

---
+++ Src/PositusClient.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Src;
+
+class PositusClient
+{
+    public function __construct()
+    {
+        return new PositusClient();
+    }
+
+    public static function init()
+    {
+        return 'test';
+    }
+}
