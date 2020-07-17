121c7fe1a4efbc27091c62fadafef55cb86eecf4
caiokawasaki <caiokawasaki@gmail.com>
2020-07-16 23:45:02 -0300

    Api paths changed to sprintf

--- src/Api/Number.php
+++ src/Api/Number.php
@@ -2,6 +2,8 @@
 
 namespace Positus\Api;
 
+use Positus\Response;
+
 class Number
 {
     private $client;
@@ -26,51 +28,51 @@ class Number
 
     public function sendMessage($data)
     {
-        return $this->sendRequest('POST', 'whatsapp/numbers/' . $this->numberId . '/messages', $data);
+        return $this->sendRequest('POST', sprintf('whatsapp/numbers/%d/messages', $this->numberId), $data);
     }
 
     public function sendTemplate($data)
     {
-        return $this->sendRequest('POST', 'whatsapp/numbers/' . $this->numberId . '/messages', $data);
+        return $this->sendRequest('POST', sprintf('whatsapp/numbers/%d/messages', $this->numberId), $data);
     }
 
     public function sendHsm($data)
     {
-        return $this->sendRequest('POST', 'whatsapp/numbers/' . $this->numberId . '/messages', $data);
+        return $this->sendRequest('POST', sprintf('whatsapp/numbers/%d/messages', $this->numberId), $data);
     }
 
     public function sendContacts($data)
     {
-        return $this->sendRequest('POST', 'whatsapp/numbers/' . $this->numberId . '/messages', $data);
+        return $this->sendRequest('POST', sprintf('whatsapp/numbers/%d/messages', $this->numberId), $data);
     }
 
     public function sendLocation($data)
     {
-        return $this->sendRequest('POST', 'whatsapp/numbers/' . $this->numberId . '/messages', $data);
+        return $this->sendRequest('POST', sprintf('whatsapp/numbers/%d/messages', $this->numberId), $data);
     }
 
     public function sendImage($data)
     {
-        return $this->sendRequest('POST', 'whatsapp/numbers/' . $this->numberId . '/messages', $data);
+        return $this->sendRequest('POST', sprintf('whatsapp/numbers/%d/messages', $this->numberId), $data);
     }
 
     public function sendDocument($data)
     {
-        return $this->sendRequest('POST', 'whatsapp/numbers/' . $this->numberId . '/messages', $data);
+        return $this->sendRequest('POST', sprintf('whatsapp/numbers/%d/messages', $this->numberId), $data);
     }
 
     public function sendVideo($data)
     {
-        return $this->sendRequest('POST', 'whatsapp/numbers/' . $this->numberId . '/messages', $data);
+        return $this->sendRequest('POST', sprintf('whatsapp/numbers/%d/messages', $this->numberId), $data);
     }
 
     public function sendAudio($data)
     {
-        return $this->sendRequest('POST', 'whatsapp/numbers/' . $this->numberId . '/messages', $data);
+        return $this->sendRequest('POST', sprintf('whatsapp/numbers/%d/messages', $this->numberId), $data);
     }
 
     public function getMedia($mediaId)
     {
-        return $this->sendRequest('GET', 'whatsapp/numbers/' . $this->numberId . '/media/' . $mediaId);
+        return $this->sendRequest('GET', sprintf('whatsapp/numbers/%d/media/%s', $this->numberId, $mediaId));
     }
 }
